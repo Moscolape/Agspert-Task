85a0d5d35b4e299c4c2d27e048d7f129bc238d67
moscolape <chunkums088@gmail.com>
2024-06-01 17:56:49 +0100

    add a redirects file for netlify deployment

--- src/utils/custom-hook.ts
+++ src/utils/custom-hook.ts
@@ -1,23 +1,30 @@
 import { useState } from "react";
 
+// Define the structure of each option
 interface Option {
-  label: string;
-  value: string | number;
+  label: string; // Label to display
+  value: string | number; // Value of the option
 }
 
+// Custom hook for managing multi-select state
 const useCustomMultiSelect = (initialOptions: Option[]) => {
+  // State variables for selected value and options
   const [value, setValue] = useState<Option[]>([]);
   const [options] = useState<Option[]>(initialOptions);
 
+  // Function to handle selection change
   const onChange = (selectedOptions: Option | Option[]) => {
     if (Array.isArray(selectedOptions)) {
+      // If multiple options are selected, set the selected options
       setValue(selectedOptions);
     } else {
+      // If a single option is selected, convert it into an array and set
       setValue([selectedOptions]);
     }
   };
 
+  // Return selected value, options, and onChange function
   return { value, options, onChange };
 };
 
-export default useCustomMultiSelect;
+export default useCustomMultiSelect; // Export the custom hook
